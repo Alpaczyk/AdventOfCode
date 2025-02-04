7157799285c3f61d72e5b16eb3aa1ea3d98d8649
Alpaczyk <michalek.czaplicki@gmail.com>
2025-02-04 22:56:18 +0100

    wr

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<List<Integer>> list = new ArrayList<>();
+        try {
+            File myObj = new File("C:/Users/micha/Desktop/AOC/input.txt");
+            Scanner myReader = new Scanner(myObj);
+            while(myReader.hasNextLine()) {
+                String line = myReader.nextLine();
+                int[] numbersInLine =  Arrays.stream(line.split(" "))
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
+                List<Integer> tempList =  new ArrayList<>(n
+            }
+            myReader.close();
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("File not found");
+        }
     }
 }
