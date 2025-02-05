5771b89ae7b009d1ab118d70e20538c28a161a69
alpaczyk <michalek.czaplicki@gmail.com>
2025-02-05 23:54:44 +0100

    Second puzzle of day 2(going bad)

--- src/Main.java
+++ src/Main.java
@@ -20,30 +20,48 @@ public class Main {
         catch(FileNotFoundException e) {
             System.out.println("File not found");
         }
-        int safe = 0;
-        boolean increase;
 
+        List<int[][]> listDamp = new ArrayList<>();
         for (int i = 0; i < list.size(); i++) {
-            increase = list.get(i)[0] < list.get(i)[1];
-            for (int j = 0; j < list.get(i).length - 1; j++) {
-                if(increase && list.get(i)[j] >= list.get(i)[j+1]) {
-                    System.out.println(i);
-                    safe--;
-                    break;
+            int[][] damp = new int[list.get(i).length + 1][list.get(i).length - 1];
+            damp[0] = list.get(i);
+            for(int j = 0; j < list.get(i).length; j++) {
+                int finalI = i;
+                int finalJ = j;
+                int[] tempList = Arrays.stream(list.get(i))
+                        .filter(a -> a != list.get(finalI)[finalJ])
+                        .toArray();
+                damp[j+1] = tempList;
                 }
-                else if(!increase && list.get(i)[j] <= list.get(i)[j+1]) {
-                    System.out.println(i);
-                    safe--;
-                    break;
-                }
-                if(Math.abs(list.get(i)[j] - list.get(i)[j+1]) < 1 || Math.abs(list.get(i)[j] - list.get(i)[j+1]) > 3) {
-                    System.out.println(i);
-                    safe--;
-                    break;
-                }
+            listDamp.add(damp);
             }
-            safe++;
+        int counter =0;
+        boolean safe;
+        boolean increase;
+        for (int l = 0; l < listDamp.size(); l++) {
+            for (int i = 0; i < listDamp.get(l).length; i++) {
+                for(int k = 0; k < listDamp.get(l)[i].length; k++){
+                    increase = listDamp.get(l)[i][j] < list.get(i)[1];
+                    for (int j = 0; j < listDamp.get(l)[i].length - 1; j++) {
+                        if(increase && list.get(i)[j] >= list.get(i)[j+1]) {
+                            safe = false;
+                            break;
+                        }
+                        else if(!increase && list.get(i)[j] <= list.get(i)[j+1]) {
+                            safe = false;
+                            break;
+                        }
+                        if(Math.abs(list.get(i)[j] - list.get(i)[j+1]) < 1 || Math.abs(list.get(i)[j] - list.get(i)[j+1]) > 3) {
+                            safe = false;
+                            break;
+                        }
+                    }
+                    safe++;
+                }
+
+            }
         }
+
         System.out.println(safe);
     }
 }
