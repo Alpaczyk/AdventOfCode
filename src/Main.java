677837ed599bf3353650c10579515068b6d33fbe
alpaczyk <michalek.czaplicki@gmail.com>
2025-02-05 23:04:08 +0100

    First Puzzle of Day 2

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,46 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<List<Integer>> list = new ArrayList<>();
+        List<int[]> list = new ArrayList<>();
         try {
-            File myObj = new File("C:/Users/micha/Desktop/AOC/input.txt");
+            File myObj = new File("C:/Users/kamil/OneDrive/Pulpit/AOC/input.txt");
             Scanner myReader = new Scanner(myObj);
             while(myReader.hasNextLine()) {
                 String line = myReader.nextLine();
                 int[] numbersInLine =  Arrays.stream(line.split(" "))
                         .mapToInt(Integer::parseInt)
                         .toArray();
-                List<Integer> tempList =  new ArrayList<>(n
+                list.add(numbersInLine);
             }
             myReader.close();
         }
         catch(FileNotFoundException e) {
             System.out.println("File not found");
         }
+        int safe = 0;
+        boolean increase;
+
+        for (int i = 0; i < list.size(); i++) {
+            increase = list.get(i)[0] < list.get(i)[1];
+            for (int j = 0; j < list.get(i).length - 1; j++) {
+                if(increase && list.get(i)[j] >= list.get(i)[j+1]) {
+                    System.out.println(i);
+                    safe--;
+                    break;
+                }
+                else if(!increase && list.get(i)[j] <= list.get(i)[j+1]) {
+                    System.out.println(i);
+                    safe--;
+                    break;
+                }
+                if(Math.abs(list.get(i)[j] - list.get(i)[j+1]) < 1 || Math.abs(list.get(i)[j] - list.get(i)[j+1]) > 3) {
+                    System.out.println(i);
+                    safe--;
+                    break;
+                }
+            }
+            safe++;
+        }
+        System.out.println(safe);
     }
 }
