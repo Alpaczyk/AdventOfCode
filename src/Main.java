53d94b7b504f5c2252a2362422aae11fa0027b5b
alpaczyk <michalek.czaplicki@gmail.com>
2025-02-06 23:33:23 +0100

    Rewrote to begin

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,15 @@ public class Main {
         try {
             File myObj = new File("C:/Users/kamil/OneDrive/Pulpit/AOC/input.txt");
             Scanner myReader = new Scanner(myObj);
-            while(myReader.hasNextLine()) {
+            while (myReader.hasNextLine()) {
                 String line = myReader.nextLine();
-                int[] numbersInLine =  Arrays.stream(line.split(" "))
+                int[] numbersInLine = Arrays.stream(line.split(" "))
                         .mapToInt(Integer::parseInt)
                         .toArray();
                 list.add(numbersInLine);
             }
             myReader.close();
-        }
-        catch(FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             System.out.println("File not found");
         }
 
@@ -25,43 +24,39 @@ public class Main {
         for (int i = 0; i < list.size(); i++) {
             int[][] damp = new int[list.get(i).length + 1][list.get(i).length - 1];
             damp[0] = list.get(i);
-            for(int j = 0; j < list.get(i).length; j++) {
+            for (int j = 0; j < list.get(i).length; j++) {
                 int finalI = i;
                 int finalJ = j;
                 int[] tempList = Arrays.stream(list.get(i))
                         .filter(a -> a != list.get(finalI)[finalJ])
                         .toArray();
-                damp[j+1] = tempList;
-                }
+                damp[j + 1] = tempList;
+            }
             listDamp.add(damp);
-            }
-        int counter =0;
-        boolean safe;
-        boolean increase;
-        for (int l = 0; l < listDamp.size(); l++) {
-            for (int i = 0; i < listDamp.get(l).length; i++) {
-                for(int k = 0; k < listDamp.get(l)[i].length; k++){
-                    increase = listDamp.get(l)[i][j] < list.get(i)[1];
-                    for (int j = 0; j < listDamp.get(l)[i].length - 1; j++) {
-                        if(increase && list.get(i)[j] >= list.get(i)[j+1]) {
-                            safe = false;
-                            break;
-                        }
-                        else if(!increase && list.get(i)[j] <= list.get(i)[j+1]) {
-                            safe = false;
-                            break;
-                        }
-                        if(Math.abs(list.get(i)[j] - list.get(i)[j+1]) < 1 || Math.abs(list.get(i)[j] - list.get(i)[j+1]) > 3) {
-                            safe = false;
-                            break;
-                        }
-                    }
-                    safe++;
-                }
-
-            }
         }
+        int safe = 0;
+        boolean increase;
 
+        for (int i = 0; i < list.size(); i++) {
+            increase = list.get(i)[0] < list.get(i)[1];
+            for (int j = 0; j < list.get(i).length - 1; j++) {
+                if (increase && list.get(i)[j] >= list.get(i)[j + 1]) {
+                    System.out.println(i);
+                    safe--;
+                    break;
+                } else if (!increase && list.get(i)[j] <= list.get(i)[j + 1]) {
+                    System.out.println(i);
+                    safe--;
+                    break;
+                }
+                if (Math.abs(list.get(i)[j] - list.get(i)[j + 1]) < 1 || Math.abs(list.get(i)[j] - list.get(i)[j + 1]) > 3) {
+                    System.out.println(i);
+                    safe--;
+                    break;
+                }
+            }
+            safe++;
+        }
         System.out.println(safe);
     }
 }
